b965a375306293a805c43b250ed3286b4d147116
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2023-11-27 14:49:08 -0600

    Construct method to calculate total value of cards

--- lesson_3/twentyone/twentyone.rb
+++ lesson_3/twentyone/twentyone.rb
@@ -3,10 +3,8 @@ require 'yaml'
 
 MESSAGES = YAML.load_file('twentyone.yml')
 
-CARD_VALUES = ('2'..'10').to_a + ['J', 'Q', 'K', 'A']
+CARD_NUMBERS = ('2'..'10').to_a + ['J', 'Q', 'K', 'A']
 SUITS = ['D', 'C', 'H', 'S']
-
-DEALER_STAY_VALUE = 17
 
 # formatting methods
 def prompt(msg)
@@ -21,7 +19,7 @@ end
 def initialize_deck
   deck = []
 
-  CARD_VALUES.each do |value|
+  CARD_NUMBERS.each do |value|
     SUITS.each { |suit| deck << [value, suit] }
   end
 
@@ -37,66 +35,121 @@ def shuffle!(deck)
 end
 
 def deal_cards(deck)
-  player_cards = [deck.pop]
-  dealer_cards = [deck.pop]
-  player_cards << deck.pop
-  dealer_cards << deck.pop
+  player_cards = []
+  dealer_cards = []
+
+  2.times do
+    player_cards << deck.pop
+    dealer_cards << deck.pop
+  end
 
   [player_cards, dealer_cards]
 end
 
+def display_cards(cards)
+
+end
+
 # calculation methods
-def calculate_total_cards_value(cards)
-  
+def calculate_total_value(cards)
+  numbers = cards.map(&:first)
+
+  total = numbers.reduce(0) do |sum, number|
+    if number == 'A'
+      sum + 11
+    elsif ['J', 'Q', 'K'].include?(number)
+      sum + 10
+    else
+      sum + number.to_i
+    end
+  end
+
+  numbers.count { |number| number == 'A' }.times do
+    total -= 10 if total > 21
+  end
+
+  total
 end
 
 def busted?(cards)
-
+  calculate_total_value(cards) > 21
 end
 
 def determine_winner(player_cards, dealer_cards)
+  player_total = calculate_total_value(player_cards)
+  dealer_total = calculate_total_value(dealer_cards)
 
+  if player_total > dealer_total
+    'Player'
+  elsif dealer_total > player_total
+    'Dealer'
+  else
+    'Push'
+  end
 end
 
 # result methods
-
+def display_result(winner)
+  case winner
+  when 'Player'
+    prompt "Player wins!"
+  when 'Dealer'
+    prompt "Dealer wins!"
+  else
+    prompt "Push!"
+  end
+end
 
 # -----------------------------------------------------------------------------
 
 # 1. Initialize deck
 deck = initialize_deck
 
-# deck -> stack-like data structure represented by 2D nested-array
+# deck -> stack-like data structure represented by 2D nested array
 
 # 2. Deal cards to player and dealer
 player_cards, dealer_cards = deal_cards(deck)
+puts "Dealer cards: #{dealer_cards}"
+puts "Score: #{calculate_total_value(dealer_cards)}"
+puts "Player cards: #{player_cards}"
+puts "Score: #{calculate_total_value(player_cards)}"
 
 # # 3. Player turn: hit or stay
 # #   - repeat until bust or "stay"
-# loop do
-#   prompt 'hit_or_stay'
-#   answer = gets.chomp
+loop do
+  prompt 'hit_or_stay'
+  answer = gets.chomp
 
-#   break if %w(s stay).include?(answer) || busted?(player_cards)
+  break if %w(s stay).include?(answer)
 
-#   player_cards << deck.pop
-# end
+  player_cards << deck.pop
+  puts "Player cards: #{player_cards}"
+  puts "Score: #{calculate_total_value(player_cards)}"
 
-# # 4. If player bust, dealer wins.
-# if busted?(player_cards)
-#   winner = 'Dealer'
-# end
+  break if busted?(player_cards)
+end
+
+# 4. If player bust, dealer wins.
+if busted?(player_cards)
+  winner = 'Dealer'
+end
 
 # # 5. Dealer turn: hit or stay
 # #   - repeat until total >= 17
-# while calculate_total_cards_value(dealer_cards) < DEALER_STAY_VALUE
-#   dealer_cards << deck.pop
-# end
+while calculate_total_value(dealer_cards) < 17
+  puts "Dealer cards: #{dealer_cards}"
+  puts "Score: #{calculate_total_value(dealer_cards)}"
+  dealer_cards << deck.pop
+end
 
 # # 6. If dealer bust, player wins.
-# if busted?(dealer_cards)
-#   winner = 'Player'
-# end
+puts "Dealer cards: #{dealer_cards}"
+puts "Score: #{calculate_total_value(dealer_cards)}"
+if busted?(dealer_cards)
+  winner = 'Player'
+end
 
 # # 7. Compare cards and declare winner.
-# winner = determine_winner(player_cards, dealer_cards)
+winner = determine_winner(player_cards, dealer_cards)
+display_result(winner)
+prompt "The winner is #{winner}!"
