a2d7ddbd34bb956e2f80514aeb948fe44a15c810
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2023-11-27 18:16:50 -0600

    Finish working prototype of Twenty-One game

--- lesson_3/twentyone/twentyone.rb
+++ lesson_3/twentyone/twentyone.rb
@@ -3,15 +3,110 @@ require 'yaml'
 
 MESSAGES = YAML.load_file('twentyone.yml')
 
-CARD_NUMBERS = ('2'..'10').to_a + ['J', 'Q', 'K', 'A']
+CARD_RANKS = ('2'..'10').to_a + ['J', 'Q', 'K', 'A']
 SUITS = ['D', 'C', 'H', 'S']
 
 # formatting methods
 def prompt(msg)
   if MESSAGES.keys.include?(msg)
-    puts MESSAGES[msg]
+    puts ">> #{MESSAGES[msg]}"
   else
     puts ">> #{msg}"
+  end
+end
+
+def display_title(title="TWENTY-ONE")
+  system 'clear'
+  top_bottom_edge = '-' * (title.length + 2)
+
+  puts "+#{top_bottom_edge}+"
+  puts "| #{title} |"
+  puts "+#{top_bottom_edge}+"
+  puts ''
+end
+
+# game methods
+def play_game
+  # 1. Initialize deck
+  deck = initialize_deck
+
+  # 2. Deal cards to player and dealer
+  cards = deal_cards(deck)
+
+  # # 3. Player turn: hit or stay
+  # #   - repeat until bust or "stay"
+  play_player_turn(cards, deck)
+
+  # 4. If player bust, dealer wins.
+  if busted?(cards[:player])
+    prompt 'player_bust'
+    sleep(2)
+    return
+  else
+    prompt 'stay'
+  end
+
+  # # 5. Dealer turn: hit or stay
+  # #   - repeat until total >= 17
+  play_dealer_turn(cards, deck)
+
+  # # 6. If dealer bust, player wins.
+  display_game(cards)
+  return if busted?(cards[:dealer])
+
+  # # 7. Compare cards and declare winner.
+  winner = determine_winner(cards)
+  display_result(winner)
+  prompt "The winner is #{winner}!"
+end
+
+def display_game(cards)
+  display_title
+
+  puts "DEALER SCORE: #{calculate_total_value(cards[:dealer])}"
+  display_cards(cards[:dealer])
+  puts ''
+
+  puts "PLAYER SCORE: #{calculate_total_value(cards[:player])}"
+  display_cards(cards[:player])
+  puts ''
+end
+
+# player_methods
+def play_player_turn(cards, deck)
+  display_game(cards)
+
+  loop do
+    choice = ask_hit_or_stay
+
+    return if %w(s stay).include?(choice)
+
+    prompt 'hit'
+    cards[:player] << deck.pop
+    display_game(cards)
+
+    return if busted?(cards[:player])
+  end
+end
+
+def ask_hit_or_stay
+  prompt 'hit_or_stay'
+
+  loop do
+    answer = gets.chomp.downcase
+
+    return answer if %w(h hit s stay).include?(answer)
+
+    prompt 'invalid_hit_or_stay'
+  end
+end
+
+# dealer_methods
+def play_dealer_turn(cards, deck)
+  while calculate_total_value(cards[:dealer]) < 17
+    cards[:dealer] << deck.pop
+    display_game(cards)
+    sleep(2)
   end
 end
 
@@ -19,8 +114,8 @@ end
 def initialize_deck
   deck = []
 
-  CARD_NUMBERS.each do |value|
-    SUITS.each { |suit| deck << [value, suit] }
+  CARD_RANKS.each do |rank|
+    SUITS.each { |suit| deck << [rank, suit] }
   end
 
   shuffle!(deck)
@@ -43,28 +138,28 @@ def deal_cards(deck)
     dealer_cards << deck.pop
   end
 
-  [player_cards, dealer_cards]
+  { player: player_cards, dealer: dealer_cards }
 end
 
 def display_cards(cards)
-
+  p cards
 end
 
 # calculation methods
 def calculate_total_value(cards)
-  numbers = cards.map(&:first)
+  ranks = cards.map(&:first)
 
-  total = numbers.reduce(0) do |sum, number|
-    if number == 'A'
+  total = ranks.reduce(0) do |sum, rank|
+    if rank == 'A'
       sum + 11
-    elsif ['J', 'Q', 'K'].include?(number)
+    elsif ['J', 'Q', 'K'].include?(rank)
       sum + 10
     else
-      sum + number.to_i
+      sum + rank.to_i
     end
   end
 
-  numbers.count { |number| number == 'A' }.times do
+  ranks.count('A').times do
     total -= 10 if total > 21
   end
 
@@ -75,21 +170,19 @@ def busted?(cards)
   calculate_total_value(cards) > 21
 end
 
-def determine_winner(player_cards, dealer_cards)
-  player_total = calculate_total_value(player_cards)
-  dealer_total = calculate_total_value(dealer_cards)
+def determine_winner(cards)
+  player_total = calculate_total_value(cards[:player])
+  dealer_total = calculate_total_value(cards[:dealer])
 
-  if player_total > dealer_total
-    'Player'
-  elsif dealer_total > player_total
+  if busted?(cards[:player]) || dealer_total > player_total
     'Dealer'
-  else
-    'Push'
+  elsif busted?(cards[:dealer]) || player_total > dealer_total
+    'Player'
   end
 end
 
 # result methods
-def display_result(winner)
+def display_result(winner=nil)
   case winner
   when 'Player'
     prompt "Player wins!"
@@ -98,58 +191,31 @@ def display_result(winner)
   else
     prompt "Push!"
   end
+
+  sleep(2)
+end
+
+def play_again?
+  prompt 'play_again'
+  answered_yes?
+end
+
+def answered_yes?
+  prompt 'yes_or_no'
+  loop do
+    answer = gets.chomp.downcase
+    return %w(y yes).include?(answer) if %w(y yes n no).include?(answer)
+
+    prompt 'invalid_yes_or_no'
+  end
 end
 
 # -----------------------------------------------------------------------------
 
-# 1. Initialize deck
-deck = initialize_deck
-
-# deck -> stack-like data structure represented by 2D nested array
-
-# 2. Deal cards to player and dealer
-player_cards, dealer_cards = deal_cards(deck)
-puts "Dealer cards: #{dealer_cards}"
-puts "Score: #{calculate_total_value(dealer_cards)}"
-puts "Player cards: #{player_cards}"
-puts "Score: #{calculate_total_value(player_cards)}"
-
-# # 3. Player turn: hit or stay
-# #   - repeat until bust or "stay"
 loop do
-  prompt 'hit_or_stay'
-  answer = gets.chomp
+  play_game
 
-  break if %w(s stay).include?(answer)
-
-  player_cards << deck.pop
-  puts "Player cards: #{player_cards}"
-  puts "Score: #{calculate_total_value(player_cards)}"
-
-  break if busted?(player_cards)
+  break unless play_again?
 end
 
-# 4. If player bust, dealer wins.
-if busted?(player_cards)
-  winner = 'Dealer'
-end
-
-# # 5. Dealer turn: hit or stay
-# #   - repeat until total >= 17
-while calculate_total_value(dealer_cards) < 17
-  puts "Dealer cards: #{dealer_cards}"
-  puts "Score: #{calculate_total_value(dealer_cards)}"
-  dealer_cards << deck.pop
-end
-
-# # 6. If dealer bust, player wins.
-puts "Dealer cards: #{dealer_cards}"
-puts "Score: #{calculate_total_value(dealer_cards)}"
-if busted?(dealer_cards)
-  winner = 'Player'
-end
-
-# # 7. Compare cards and declare winner.
-winner = determine_winner(player_cards, dealer_cards)
-display_result(winner)
-prompt "The winner is #{winner}!"
+prompt 'goodbye'
